a7416bb12610f9333a2227e0b1899f775c23b4d9
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-14 16:15:42 +0900

    Create 3주3차3.java

---
+++ java/03주차/3주3차과제/3주3차3.java
@@ -0,0 +1,16 @@
+package org.javaro.lecture;
+
+public class Ex1_4 {
+
+    public static void main(String[] args) {
+        System.out.println("홍길동"+"-"+"20XX0901" + "-" + "변수");
+        
+        int a = 10; // int 정수형, 이름: a, 10대입
+        int b = 20; // int 정수형, 이름: b, 20대입
+        
+        int c; //int 정수형, 이름: c, 초기화 없음
+        c = a + b; 
+        System.out.println("변수 타입별 테스트");
+        System.out.println("b의 값: " + b + "의 꿈은 " + c + " 입니다.");
+    }
+}
