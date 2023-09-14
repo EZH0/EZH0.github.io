261e1a1ec08c05653737f5ba68989c9c79ce2d14
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-14 16:07:55 +0900

    Create 3주3차2.java

---
+++ java/03주차/3주3차과제/3주3차2.java
@@ -0,0 +1,12 @@
+package org.javaro.lecture;
+
+public class Ex1_3 {
+
+    public static void main(String[] args) {
+        System.out.println("홍길동" + "_" + "20XX0901" + "_" + "변수");
+
+        int grade = 2;
+        System.out.println("소드내 로컬 변수는 초기화가 되지 않으면 에러 발생!");
+
+    }
+}
