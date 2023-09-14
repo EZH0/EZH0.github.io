b9f8285a4da79e6bb6d53c41733b96c8341e7e08
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-14 16:19:11 +0900

    Update 3주3차3.java

--- java/03주차/3주3차과제/3주3차3.java
+++ java/03주차/3주3차과제/3주3차3.java
@@ -11,6 +11,6 @@ public class Ex1_4 {
         int c; //int 정수형, 이름: c, 초기화 없음
         c = a + b; 
         System.out.println("변수 타입별 테스트");
-        System.out.println("b의 값: " + b + "의 꿈은 " + c + " 입니다.");
+        System.out.println("포스트잇 a의 값"+a+"와"+"포스트잇 b의 값"+b+"의 합은"+c+"입니다.");
     }
 }
