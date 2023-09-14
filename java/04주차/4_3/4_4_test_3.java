125c1b9401c5dca1f7413569a36a19a5c2d57101
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-14 21:48:37 +0900

    Create 4_4_test_3.java

---
+++ java/04주차/4_3/4_4_test_3.java
@@ -0,0 +1,13 @@
+package org.javaro.lecture;
+
+public class test2_3 {
+    public static void main(String[] args) {
+        int x = 6, y = 7;
+        
+        System.out.println("!(x>6)" + !(x>6));
+        System.out.println("(x == 6 && x == 5)_" + (x == 6 && x == 5));
+        System.out.println("(x == 6 || x == 5)" + (x == 6 || x == 5));
+        System.out.println("(x > -1 && y<10)" + (x > -1 && y<10));
+        System.out.println("(x > 10 && x < 5)" + (x > 10 && x < 5));
+    }
+}
