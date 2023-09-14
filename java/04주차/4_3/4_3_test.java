ebf62f4f47db1f231032504c840bb8258647b029
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-14 20:58:33 +0900

    Create 4_3_test.java

---
+++ java/04주차/4_3/4_3_test.java
@@ -0,0 +1,18 @@
+package org.javaro.lecture;
+
+public class test2_1 {
+	public static void main(String[] args) {
+		int a = 10;
+		a++;
+		System.out.println(a);
+		System.out.println(++a);
+		System.out.println(a++);
+		
+		System.out.println(1%3);
+		System.out.println(2%3);
+		System.out.println(3%3);
+		System.out.println(4%3);
+		System.out.println(5%3);
+		System.out.println(6%3);
+	}
+}
