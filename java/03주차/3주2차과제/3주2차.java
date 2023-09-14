e281d7689cd2e6efe6860fb6a32660e103af5f7f
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-14 15:53:21 +0900

    Create 3주2차.java

---
+++ java/03주차/3주2차과제/3주2차.java
@@ -0,0 +1,13 @@
+package org.javaro.mybookstore;
+
+public class Ex {
+public static void main(String [] args) {
+	System.out.println("홍길동"+"-"+"20XX0901"+"-"+"변수");
+	int grade = 3;
+	System.out.println(grade+"학년"+"홍길동");
+	System.out.println(grade+"학년"+"이몽룡");
+	System.out.println(grade+"학년"+"성춘향");
+	System.out.println(grade+"학년"+"변학도");
+	}
+
+}
