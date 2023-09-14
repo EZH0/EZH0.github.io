992e1ef822f547cf5a06f1b7dbfaf5f50b7a85d5
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-14 19:23:07 +0900

    Create 4_2_2.java

---
+++ java/04주차/4_2/4_2_2.java
@@ -0,0 +1,26 @@
+package org.javaro.lecture;
+import java.math.BigDecimal;
+
+public class Ex2_3 {
+
+    public static void main(String[] args) {
+        float a;
+        float b = 2.1f;
+        int c = 2;
+
+        a = b + c; 
+        System.out.println("b=2.1f; c=2; a=b+c=" + a);
+
+        a = b - c;
+        System.out.println("b=2.1f; c=2; a=b-c=" + a); // 0.09999905
+
+        a = b * c;
+        System.out.println("b=2.1f; c=2; a=b*c=" + a);
+
+        a = b / c;
+        System.out.println("b=2.1f; c=2; a=b/c=" + a);
+
+        a = b % c;
+        System.out.println("b=2.1f; c=2; a=b%c=" + a); // 0.09999905
+    }
+}
