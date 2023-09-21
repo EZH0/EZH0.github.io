3ea95cfee809860a2bb6a4f4ec35d2285f19cd2a
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-21 11:16:57 +0900

    Create 5_4_test_1.java

---
+++ java/05주차/5_4/5_4_test_1.java
@@ -0,0 +1,13 @@
+package org.javaro.lecture;
+
+public class Ex4_test_1 {
+	public static void main(String[] args) {
+		 int i = 1;
+	     int sum = 0;
+	        
+	     while (i <= 100) {
+	    	 sum += i++;
+	     }
+	     System.out.println("1부터 100까지 합=" + sum);
+    }
+}
