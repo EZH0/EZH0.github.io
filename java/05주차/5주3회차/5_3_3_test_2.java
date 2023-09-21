2f9ed4cedfc994543af0b2b2b17f11a6aeaa0b8d
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-21 10:57:58 +0900

    Create 5_3_3_test_2.java

---
+++ java/05주차/5주3회차/5_3_3_test_2.java
@@ -0,0 +1,16 @@
+package org.javaro.lecture;
+
+public class Ex3_test_4 {
+	 public static void main(String args[]) {
+		 String gender = "Male";
+
+		 switch (gender) {
+		     case "Male":
+		         System.out.println("남자");
+		         break;
+		     default:
+		         System.out.println("남자아님");
+		         break;
+		 }
+	 }
+}
