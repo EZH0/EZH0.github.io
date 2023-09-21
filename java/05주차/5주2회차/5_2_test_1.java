70a331691a6597b2f1406edba2eb80ab615cb3f8
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-21 10:28:26 +0900

    Create 5_2_test_1.java

---
+++ java/05주차/5주2회차/5_2_test_1.java
@@ -0,0 +1,20 @@
+package org.javaro.lecture;
+
+public class Ex3_test_1 {
+	public static void main(String[] args) {
+		int score = 90;
+
+		if (score >= 90) {
+		    System.out.println("학점=A");
+		} 
+		else if (score >= 80) {
+		    System.out.println("학점=B");
+		} 
+		else if (score >= 70) {
+		    System.out.println("학점=C");
+		} 
+		else {
+		    System.out.println("학점=F");
+		}
+    }
+}
