0dffc1e95bfc3353626c5c7a567c99862b5edc23
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-27 11:12:16 +0900

    Create 6_1_1.java

---
+++ java/06주차/6_1/6_1_1.java
@@ -0,0 +1,11 @@
+package org.javaro.lecture;
+
+public class Ex4_3 {
+
+    public static void main(String[] args) {
+        int End = 15;
+        for (int x = 11; x <= End; x++) {
+            System.out.println("x=" + x + ", End=" + End + "," + (x <= End));
+        }
+    }
+}
