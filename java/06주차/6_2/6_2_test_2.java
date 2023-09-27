ebaa9000b67c8bbc1a6c1bc7c71224a04380c814
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-27 11:30:02 +0900

    Create 6_2_test_2.java

---
+++ java/06주차/6_2/6_2_test_2.java
@@ -0,0 +1,15 @@
+package org.javaro.lecture;
+
+public class Ex4_test_4 {
+	public static void main(String[] args) {
+        final int N = 30;
+        for(int i = N;i>0;i--)
+        {
+        	for(int j = i;j>0;j--)
+        	{
+        		System.out.print("*");
+        	}
+        	System.out.println("*");
+        }
+    }
+}
