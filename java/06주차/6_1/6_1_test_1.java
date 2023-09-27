24a3d0f8311c441c6af53e3b6fef5420750856d9
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-09-27 11:17:37 +0900

    Create 6_1_test_1.java

---
+++ java/06주차/6_1/6_1_test_1.java
@@ -0,0 +1,9 @@
+package org.javaro.lecture;
+
+public class Ex4_test_1 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++) {
+        	System.out.println("i의 값= "+i);
+        }
+    }
+}
