a08714b6320093e8b3b74de1a221e2a46af29ce0
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-10-05 11:22:27 +0900

    Create 7_2_1.java

---
+++ java/07주차/7_2/7_2_1.java
@@ -0,0 +1,9 @@
+package org.javaro.lecture;
+
+public class Ex7_0 {
+    public static void main(String[] args) {
+    	NameX kdhong = new NameX();
+        kdhong.setName("홍길동");
+        System.out.println(kdhong.getSName());
+    }
+}
