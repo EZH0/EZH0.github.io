b3a2d66a054b3cce28925979e926ab31e1a567ca
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-10-05 10:55:42 +0900

    Create 7_1_4.java

---
+++ java/07주차/7_1/7_1_4.java
@@ -0,0 +1,13 @@
+package org.javaro.lecture;
+
+public class Ex6_7 {
+    
+    public static void main(String[] args) {
+        double x, y; // 값1, 값2
+        x = 10.0;
+        y = 20.0;
+
+        double sub_value = x - y;
+        System.out.println("빼기=" + sub_value);
+    }
+}
