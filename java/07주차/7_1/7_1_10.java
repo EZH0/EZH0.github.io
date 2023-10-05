c580ad384061493296bdabcb2d285744d68e52d6
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-10-05 11:16:35 +0900

    Create 7_1_10.java

---
+++ java/07주차/7_1/7_1_10.java
@@ -0,0 +1,13 @@
+package org.javaro.lecture;
+
+class Ex6_13 {
+
+    public static void main(String[] args) {
+        double x, y; // 밑변, 높이
+        x = 10.0;
+        y = 20.0;
+
+        double area = 0.5 * x * y;
+        System.out.println("면적=" + area);
+    }
+}
