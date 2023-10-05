571dd34eaa87e61abb6766046c9d162ba0982f30
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-10-05 11:05:52 +0900

    Create 7_1_6.java

---
+++ java/07주차/7_1/7_1_6.java
@@ -0,0 +1,13 @@
+package org.javaro.lecture;
+
+public class Ex6_9 {
+    
+    public static void main(String[] args) {
+        double x, y; // 값1, 값2
+        x = 10.0;
+        y = 20.0;
+        
+        double mult_value = x * y;
+        System.out.println("곱하기=" + mult_value);
+    }
+}
