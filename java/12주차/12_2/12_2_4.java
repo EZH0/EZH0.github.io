6fbf736999d0c3af8fe9dd7ef9c2685964157ec2
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-11-16 10:49:21 +0900

    Create 12_2_4.java

---
+++ java/12주차/12_2/12_2_4.java
@@ -0,0 +1,25 @@
+package org.javaro.lecture;
+import java.io.*;
+class Ex11_8 {
+
+    public static void main(String[] args) {
+        String s = new String();
+        String t = new String();
+        try {
+            FileInputStream fin = new FileInputStream("C:\\Users\\Public\\Documents\\write.txt");
+            int c;
+
+            while((c=fin.read())!=-1) {
+                s = s + (char)c;
+                t = t + c;
+            }
+            System.out.println(s);
+            System.out.println(t);
+            fin.close();
+        } catch(IOException ioe) {
+            System.out.println("파일 입출력 오류");
+        } catch(Exception e) {
+            System.out.println("파일 입출력 예외 발생");
+        }
+    }
+}
