4e418b9500e66d4cbb053678b65d0c1f43f89eeb
EZH0 <126746855+EZH0@users.noreply.github.com>
2023-11-16 10:14:00 +0900

    Create 12_1_4.java

---
+++ java/12주차/12_1/12_1_4.java
@@ -0,0 +1,9 @@
+package org.javaro.lecture;
+import java.io.*;
+
+class Ex11_4 {
+    public static void main(String[] args) {
+        byte[] data = {75, 79, 82, 69, 65};
+        for(int i=0; i<5; i++) System.out.print((char)data[i]);
+    }
+}
